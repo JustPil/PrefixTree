66530d8021b39e78042eb594644fd6d4b393d1f4
JustPil <88640664+JustPil@users.noreply.github.com>
2022-07-08 18:13:10 -0700

    Delete src/trie directory

--- src/trie/TrieInterface.java
+++
@@ -1,9 +0,0 @@
-package trie;
-
-public interface TrieInterface {
-    Node ROOT = new Node('*');
-    boolean addWord(String word);
-    boolean deleteWord(String word);
-    boolean searchWord(String word);
-    boolean searchPrefix(String prefix);
-}
