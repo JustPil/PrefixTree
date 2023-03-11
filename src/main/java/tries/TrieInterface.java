d28ff0f4992dbb7edb787ed7396d74f69a3e83f8
JustPil <88640664+JustPil@users.noreply.github.com>
2023-03-10 17:40:27 -0800

    Delete src directory

--- src/main/java/tries/TrieInterface.java
+++
@@ -1,8 +0,0 @@
-package tries;
-
-public interface TrieInterface {
-    void addWord(String word);
-    boolean deleteWord(String word);
-    boolean searchWord(String word);
-    boolean searchPrefix(String prefix);
-}
